6abdaaddf5951c19a156d5429084716a63161ffa
jcacc <joe@accardi.xyz>
2023-04-02 15:11:24 -0400

    screen pet project

--- screen_pet.py
+++ screen_pet.py
@@ -15,6 +15,34 @@ def blink():
     root.after(250, toggle_eyes)
     root.after(3000,blink)
 
+def toggle_pupils():
+    if not c.eyes_crossed:
+        c.move(pupil_left, 10, -5)
+        c.move(pupil_right, -10, -5)
+        c.eyes_crossed = True
+    else:
+        c.move(pupil_left, -10, 5)
+        c.move(pupil_right, 10, 5)
+        c.eyes_crossed = False       
+
+def cheeky(event):
+    toggle_tongue()
+    toggle_pupils()
+    hide_happy(event)
+    root.after(1000, toggle_tongue)
+    root.after(1000, toggle_pupils)
+    return
+
+
+def toggle_tongue():
+    if not c.tongue_out:
+        c.itemconfigure(tongue_tip, state=NORMAL)
+        c.itemconfigure(tongue_main, state=NORMAL)
+        c.tongue_out = True
+    else:
+        c.itemconfigure(tongue_tip, state=HIDDEN)
+        c.itemconfigure(tongue_main, state=HIDDEN)
+        c.tongue_out = False
 def show_happy(event):
     if (20 <= event.x <= 350) and (20 <= event.y <= 350):
         c.itemconfigure(cheek_left, state=NORMAL)
@@ -23,7 +51,14 @@ def show_happy(event):
         c.itemconfigure(mouth_normal, state=HIDDEN)
         c.itemconfigure(mouth_sad, state=HIDDEN)
     return
-    
+def hide_happy(event):
+    c.itemconfigure(cheek_left, state=HIDDEN)
+    c.itemconfigure(cheek_right, state=HIDDEN)
+    c.itemconfigure(mouth_happy, state=HIDDEN)
+    c.itemconfigure(mouth_normal, state=NORMAL)
+    c.itemconfigure(mouth_sad, state=HIDDEN)
+    return
+
 
 
 
@@ -33,7 +68,7 @@ c = Canvas(root, width=400, height=400)
 c.configure(bg='dark blue', highlightthickness=0)
 
 
-c.body_color = 'SkyBlue1'
+c.body_color = 'red'
 body = c.create_oval(35, 20, 365, 350, outline=c.body_color, fill=c.body_color)
 ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, outline=c.body_color, fill=c.body_color)
 ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, fill=c.body_color)
@@ -45,14 +80,27 @@ pupil_left = c.create_oval(140, 145, 150, 155, outline='black', fill='black')
 eye_right = c.create_oval(230, 110, 260, 170, outline='black', fill='white')
 pupil_right = c.create_oval(240, 145, 250, 155, outline='black', fill='black')
 
-mouth_normal = c.create_line(170, 250, 200, 272, 230, 250, smooth=1, width=2, state=NORMAL)
+mouth_normal = c.create_line(170, 250, 200, 252, 230, 250, smooth=1, width=2, state=NORMAL)
 mouth_happy = c.create_line(170, 250, 200, 282, 230, 250, smooth=1, width=2, state=HIDDEN)
 mouth_sad = c.create_line(170, 250, 200, 232, 230, 250, smooth=1, width=2, state=HIDDEN)
+
+tongue_main = c.create_rectangle(170, 250, 230, 290, outline='DeepPink3', fill='DeepPink3', state=HIDDEN)
+tongue_tip = c.create_oval(170, 285, 230, 300, outline='DeepPink3', fill='DeepPink3', state=HIDDEN)
 
 cheek_left = c.create_oval(70, 180, 120, 230, outline='pink', fill='pink', state=HIDDEN)
 cheek_right = c.create_oval(280, 180, 330, 230, outline='pink', fill='pink', state=HIDDEN)
 
 c.pack()
+
+c.bind('<Motion>', show_happy)
+c.bind('<Leave>', hide_happy)
+c.bind('<Double-1>', cheeky)
+
+
+c.eyes_crossed = False
+c.tongue_out = False
+
+
 root.after(1000,blink)
 root.mainloop()
 
